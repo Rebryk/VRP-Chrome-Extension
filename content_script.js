3b07839acf268c6c62287f896391b4f2607c490a
BioRyajenka <igor-sushencev@yandex.ru>
2017-10-21 10:17:22 +0300

    Prettify even more

--- content_script.js
+++ content_script.js
@@ -1,8 +1,20 @@
-var MAX_STORAGE_BYTES = 1024;
+var CACHE_SIZE = 100; // maximum stored voices
+
+function storeInCache(key, val) {
+    chrome.storage.local.set({key: val});
+    // TODO: limit cache size
+}
+
+function retrieveFromCache(key, callback) {
+    chrome.storage.local.get(key, function(res) {callback(res[key]);});
+}
 
 document.arrive(".im_msg_audiomsg", {existing: true}, function() {
     var messageId = this.id;
     var voiceUrl = this.getElementsByClassName("audio-msg-track")[0].getAttribute('data-ogg');
+    
+    console.log('Message id is ' + messageId);
+    console.log('Voice url is ' + voiceUrl);
 
     var iconAndText = document.createElement("div");
     var loadingIcon = document.createElement("div");
@@ -24,11 +36,13 @@ document.arrive(".im_msg_audiomsg", {existing: true}, function() {
         }
     }
     
-    chrome.storage.local.get(messageId, function (storedMessage) {
-        if (Object.keys(storedMessage).length !== 0) {
-            console.log("Found " + storedMessage);
-            showResult(storedMessage[messageId]);
+    retrieveFromCache(messageId, function (storedMessage) {
+        console.log('storedMessage is ' + storedMessage);
+        if (storedMessage) {
+            console.log('Found ' + storedMessage + ' in cache');
+            showResult(storedMessage);
         } else {
+            console.log('Not found. Sending request.')
             var xhr = new XMLHttpRequest();
             xhr.open('post', 'https://vrp.eu.ngrok.io/recognize', true);
             xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
@@ -36,10 +50,7 @@ document.arrive(".im_msg_audiomsg", {existing: true}, function() {
             xhr.onreadystatechange = function () {
                 if (xhr.readyState === 4 && xhr.status === 200) {
                     showResult(xhr.responseText);
-                    console.log(messageId);
-                    var objToSave = {};
-                    objToSave[messageId] = xhr.responseText;
-                    chrome.storage.local.set(objToSave);
+                    storeInCache(messageId, xhr.responseText);
                 } else {
                     showResult('Проблемы с соединением. Попробуйте перезагрузить страницу.', true);
                 }
