c5c8c672998b636b56fafb52412a5b4f5a3a78d5
BioRyajenka <igor-sushencev@yandex.ru>
2017-10-21 09:51:57 +0300

    Beautify code

--- content_script.js
+++ content_script.js
@@ -2,52 +2,49 @@ var MAX_STORAGE_BYTES = 1024;
 
 document.arrive(".im_msg_audiomsg", {existing: true}, function() {
     var messageId = this.id;
+    var voiceUrl = this.getElementsByClassName("audio-msg-track")[0].getAttribute('data-ogg');
 
     var iconAndText = document.createElement("div");
     var loadingIcon = document.createElement("div");
     loadingIcon.className = "loader";
-	var text = document.createElement("span");
-	text.className = "loading_text";
-	text.appendChild(document.createTextNode("Войс обрабатывается. Подождите..."));
-	console.log(messageId);
-	iconAndText.appendChild(loadingIcon);
-    iconAndText.appendChild(text);
+    var textNode = document.createElement("span");
+    textNode.className = "loading_text";
+    textNode.appendChild(document.createTextNode("Войс обрабатывается. Подождите..."));
+    console.log(messageId);
+    iconAndText.appendChild(loadingIcon);
+    iconAndText.appendChild(textNode);
     this.appendChild(iconAndText);
-	var that = this;
+    
+    function showResult(resultText, isError) {
+        loadingIcon.remove();
+        textNode.innerHTML = '';
+        textNode.appendChild(document.createTextNode(resultText));
+        if (isError) {
+            textNode.className = "error_text";
+        }
+    }
+    
     chrome.storage.local.get(messageId, function (storedMessage) {
-    	if (Object.keys(storedMessage).length !== 0) {
-			console.log("Found " + storedMessage);
-            loadingIcon.remove();
-            text.innerHTML = '';
-            text.appendChild(document.createTextNode(storedMessage[messageId]));
+        if (Object.keys(storedMessage).length !== 0) {
+            console.log("Found " + storedMessage);
+            showResult(storedMessage[messageId]);
         } else {
-            var trackNode = that.getElementsByClassName("audio-msg-track")[0];
-            var voiceUrl = trackNode.getAttribute('data-ogg');
-
             var xhr = new XMLHttpRequest();
             xhr.open('post', 'https://vrp.eu.ngrok.io/recognize', true);
             xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
 
-            var so = that;
-            // send the collected data as JSON
             xhr.onreadystatechange = function () {
                 if (xhr.readyState === 4 && xhr.status === 200) {
-                    //so.appendChild(document.createTextNode(xhr.responseText));
-                    loadingIcon.remove();
-                    text.innerHTML = '';
-                    text.appendChild(document.createTextNode(xhr.responseText));
+                    showResult(xhr.responseText);
                     console.log(messageId);
                     var objToSave = {};
                     objToSave[messageId] = xhr.responseText;
                     chrome.storage.local.set(objToSave);
+                } else {
+                    showResult('Проблемы с соединением. Попробуйте перезагрузить страницу.', true);
                 }
             };
-
             xhr.send('{"url": "' + voiceUrl + '"}');
         }
     });
-
-	//xhr.onloadend = function () {
-	//	this.appendChild(document.createTextNode(xhr.responseText));
-	//};
 });
